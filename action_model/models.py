fix a bug in forward_with_cfg, which cause a slight decrease in average performance during inference when using cfg

action_model/models.py
@@ -259,9 +259,10 @@ class DiT(nn.Module):
         half = x[: len(x) // 2]
         combined = torch.cat([half, half], dim=0).to(next(self.x_embedder.parameters()).dtype)
         model_out = self.forward(combined, t, z)
-        
-        eps, rest = model_out[:, :self.in_channels], model_out[:, self.in_channels:]
+        # eps, rest = model_out[:, :self.in_channels], model_out[:, self.in_channels:]
+        eps, rest = model_out[:, :, :self.in_channels], model_out[:, :, self.in_channels:]
         cond_eps, uncond_eps = torch.split(eps, len(eps) // 2, dim=0)
         half_eps = uncond_eps + cfg_scale * (cond_eps - uncond_eps)
         eps = torch.cat([half_eps, half_eps], dim=0)
-        return torch.cat([eps, rest], dim=1)
+        # return torch.cat([eps, rest], dim=1)
+        return torch.cat([eps, rest], dim=2)